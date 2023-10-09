11c2afbcf08186dd6ca520b9bb968d28e4c6c218
Eugene Krivinsky <silver313@mail.ru>
2023-10-09 13:53:33 +0300

    выполнены задания -  Task1,  Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    public static long directorySize = 0;
+
+    static void Main(string[] args)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(args[0]);
+
+        if (directoryInfo.Exists)
+        {
+            GetDirectoriesSize(directoryInfo);
+            GetFilesSize(directoryInfo);
+        } else
+        {
+            Console.WriteLine($"Папки по пути {args[0]} не существует");
+        }
+        Console.WriteLine($"Размер папки: {directorySize}");
+    }
+
+    private static void GetFilesSize(DirectoryInfo directoryInfo)
+    {
+        FileInfo[] files = directoryInfo.GetFiles();
+
+        foreach (FileInfo file in files)
+        {
+            directorySize += file.Length;
+        }
+    }
+
+    private static void GetDirectoriesSize(DirectoryInfo directoryInfo)
+    {
+        DirectoryInfo[] currentDirectoryInfo = directoryInfo.GetDirectories();
+
+        foreach (DirectoryInfo currentDirectory in currentDirectoryInfo)
+        {
+            GetDirectoriesSize(currentDirectory);
+            GetFilesSize(currentDirectory);
+        }
+    }
+}
